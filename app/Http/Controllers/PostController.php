d83c7f572b434b181f386749c0400e57da3380dd
arsachy06 <arsacahaya.upnvj@gmail.com>
2023-05-07 19:50:16 +0700

    test commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+
+class PostController extends Controller
+{
+    public function index(): View
+    {
+        //get posts
+        $posts = Post::latest()->paginate(5);
+
+        //render view with posts
+        return view('posts.index', compact('posts'));
+    }
+
+    public function create(): View
+    {
+        return view('posts.create');
+    }
+}
